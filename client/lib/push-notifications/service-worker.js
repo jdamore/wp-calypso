fa36826c4cd3ba21c903db6f2d65336c4d2fc08b
Miklos Juhasz <miklos.juhasz@automattic.com>
2016-05-23 12:42:02 +0200

    Add browser push notification prompt.

---
+++ client/lib/push-notifications/service-worker.js
@@ -0,0 +1,20 @@
+/**
+ *  This file is served as-is as /service-worker.js
+ **/
+
+/* eslint-disable */
+'use strict';
+/* eslint-enable */
+
+self.addEventListener( 'push', function( event ) {
+	var notification = event.data.json();
+
+	event.waitUntil(
+		// notifications look best with just a title
+		self.registration.showNotification( notification.msg, {
+			tag: 'note_' + notification.note_id,
+			icon: notification.icon,
+			timestamp: notification.note_timestamp,
+		} )
+	);
+} );
